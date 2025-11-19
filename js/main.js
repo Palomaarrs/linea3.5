8db88a6c06221164a2ac9447682b39334e17b938
Paloma <parreola@centro.edu.mx>
2025-11-19 10:33:35 -0600

    flecha updates

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@ items.forEach(function (item) {
     new Waypoint.Inview({
         element: item,
         enter: function (direction) {
-            item.classList.add('inview');
             console.log('Enter triggered with direction ' + direction)
         },
         entered: function (direction) {
+            item.classList.add('inview');
             console.log('Entered triggered with direction ' + direction)
         },
         exit: function (direction) {
-            item.classList.remove('inview');
             console.log('Exit triggered with direction ' + direction)
         },
         exited: function (direction) {
+            item.classList.remove('inview');
             console.log('Exited triggered with direction ' + direction)
         }
     })
