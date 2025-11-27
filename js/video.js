958b7711be14758c1f59e19fae936d9e2794b63b
JuanFuent.es <input@juanfuent.es>
2025-11-27 11:52:08 -0600

    debug video

--- js/video.js
+++ js/video.js
@@ -1,12 +1,16 @@
 const progressBtn = document.getElementById('video-progress');
 const video = document.querySelector('video');
+const duration = video.duration;
 
 progressBtn.addEventListener('input', () => {
     video.currentTime = progressBtn.value;
+    console.log("progressBtn.value", progressBtn.value);
 });
 
 video.addEventListener('timeupdate', () => {
-    progressBtn.value = Math.round(video.currentTime);
+    const progress = (video.currentTime / duration) * 100;
+    progressBtn.value = parseInt(progress.toFixed(2));
+    console.log("progress", progress);
 });
 
 video.addEventListener('ended', () => {
