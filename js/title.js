0ec549b3ffe3583273b144bbc97c86b74d779409
Paloma <parreola@centro.edu.mx>
2025-12-03 14:58:15 -0600

    camios

--- js/title.js
+++ js/title.js
@@ -1,1 +1,11 @@
  const title=document.querySelector('.fijo');
+    window.addEventListener('scroll',function(){
+        const scrollY=window.scrollY;
+        const maxScroll=document.body.getBoundingClientRect().height-window.innerHeight;
+        // Maxscroll=900 
+
+        // x= scrollY * 800/maxScroll
+        const x=scrollY * 800 / maxScroll;
+        title.style.fontVariationSettings=`'wght' ${100 + x}`;
+        console.log(x);
+    });
